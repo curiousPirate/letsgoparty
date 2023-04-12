e79d65820a6cfd22722018f7c6278f0a4b0078b5
Chaitanya Varma <gchaitanya.369@gmail.com>
2023-04-12 23:42:41 +0530

    about section update

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -9,23 +9,22 @@ function scrollToSearch() {
 
 const About = () => {
     return (
-      <div className="relative flex flex-col items-center justify-center max-h-fit sm:h-screen">
+      <div className="relative flex flex-col items-center justify-center h-screen sm:h-screen">
         <div className="absolute w-full h-full flex flex-col justify-between" id="home">
           <img src={require("../design/bg-img.jpg")} alt="bg-pic" className="h-screen object-cover sm:object-cover"/>
         </div>
         <div className="z-20 w-full max-w-screen-lg flex flex-col items-center lg:justify-end sm:justify-end">
-          <div className="shadow-2xl rounded-lg w-full sm:w-4/5 md:h-96 lg:h-full bg-cover bg-center flex sm:flex-row flex-col m-0 lg:m-100">
-            <div className="sm:w-1/2 flex flex-col justify-center items-center">
-                <div className="border-l-4 border-gray-400 p-8 text-center">
-                  <p className="italic text-cyan-950 sm:text-white sm:text-4xl md:text-2xl lg:text-6xl uppercase font-semibold">
-                  <br></br><br></br> 
+          <div className="shadow-2xl rounded-lg w-full sm:w-4/5 md:h-96 lg:h-full bg-cover bg-center flex sm:flex-row flex-col lg:m-100">
+            <div className="sm:w-1/2 flex flex-col justify-end">
+                <div className="border-l-4 border-gray-400 pl-6">
+                  <h2 className="italic text-cyan-950 text-5xl sm:text-4xl md:text-2xl lg:text-6xl uppercase font-semibold">
                   Used by nomads across the world!
-                  </p>
+                  </h2>
                 </div>
               <div className="border-t-4 border-gray-400 w-4/5 mx-auto my-4 sm:my-8"></div>
             </div>
             <div className="sm:w-1/2 bg-cyan-950 bg-opacity-50 p-8 rounded-lg flex flex-col justify-end">
-              <h3 className="text-white text-md md:text-xl lg:text-xl text-justify">
+              <h3 className="text-white text-lg md:text-xl lg:text-xl text-justify">
                 Whether you're looking for the hottest events, the trendiest clubs, unforgettable concerts, wild raves, or mouth-watering food events, we've got you covered. Our app is your ultimate guide to the best experiences the world has to offer. We guarantee you'll love life even more after using it! Experience life!
               </h3>
               <button className="opacity-75 bg-gray-100 hover:bg-cyan-900 hover:text-white text-sm font-bold py-2 px-4 rounded inline-flex items-center self-end" type="button" onClick={() => scrollToSearch()}>
