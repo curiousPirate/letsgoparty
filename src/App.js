f6258fa82161496bf1bc59e435b85acba45e3cff
Chaitanya Varma <gchaitanya.369@gmail.com>
2023-04-13 16:24:34 +0530

    changed gradient

--- src/App.js
+++ src/App.js
@@ -14,16 +14,16 @@ const [setFilter] = useState("");
     <div className="App">
       <Header />
       <About />
-      <div className="bg-gradient-to-b from-slate-950 to-sky-950 bg-no-repeat bg-bottom">
+      <div className="bg-slate-950 bg-gradient-to-b from-slate-950 to-sky-950 bg-no-repeat bg-bottom">
         <SearchBar setFilter={setFilter} />
       </div>
-      <div className="bg-gradient-to-b from-sky-950 to-white bg-no-repeat bg-bottom">
+      <div className="bg-sky-950 bg-gradient-to-b from-sky-950 to-white bg-no-repeat bg-bottom">
         <SavedCards />
       </div>
-      <div className="bg-gradient-to-b from-white to-sky-950 bg-no-repeat bg-bottom">
+      <div className="bg-white bg-gradient-to-b from-white to-sky-950 bg-no-repeat bg-bottom">
         <Newsletter />
       </div>
-      <div className="bg-gradient-to-t from-sky-950 to-sky-950 bg-no-repeat bg-bottom">
+      <div className="bg-sky-950 bg-gradient-to-t from-sky-950 to-sky-950 bg-no-repeat bg-bottom">
         <ReactFooter />
       </div>
     </div>
